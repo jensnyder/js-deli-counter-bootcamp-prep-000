088e8ea51df8b5bd1c4eb550c51dd4f8f89af960
Jennifer Snyder <jlsnyder09@gmail.com>
2018-04-22 13:42:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return `Currently serving ${katzDeliLine.shift()}.`;
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  }
+  while (katzDeliLine.length > 0) {
+    var i = 0;
+    var place = i + 1;
+    console.log(`The line is currently ${place}. ${katzDeliLine[i]}.`);
+    i++;
+  }
+}
